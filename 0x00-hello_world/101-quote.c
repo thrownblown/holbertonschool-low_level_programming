1cba2e81ad51f91931d284d343351e1664dd10b8
Alex Farioletti <alex@tcbcourier.com>
2018-06-20 23:05:52 +0000

    removed printf from comment

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
- * uses printf to make text appear
+ * uses write not to make text appear in the stderr
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
