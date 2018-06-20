d8ebd5b972064a6c071d37f3dc4b3b3a0dbc3f49
Alex Farioletti <alex@tcbcourier.com>
2018-06-20 22:32:03 +0000

    i know this wont go

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <errno.h>
+
+/**
+ * main - Entry point
+ * uses printf to make text appear
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
