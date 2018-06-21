96279c1bfdb0ddafb373fe1ea64a7c8311698e65
alex farioletti <gsf.nyc@gmail.com>
2018-06-21 13:04:14 -0700

    switch order

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,16 +14,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
+
+	int m;
+
+	m = n % 10;
+	if (m > 5)
 	{
-		printf("Last digit of %i is and is 0\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
-	else if (n > 5)
+	else if (m == 0)
 	{
-		printf("Last digit of %i is and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	} else
 	{
-		printf("Last digit of %i is and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	return (0);
 }
