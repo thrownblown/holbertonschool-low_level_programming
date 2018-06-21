004280f53d1605b893264deea894117edc468c6b
alex farioletti <gsf.nyc@gmail.com>
2018-06-21 15:04:06 -0700

    reset m to 48

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *  prints the sizes of various data types
+ * main - prints the numbers 00 and above and below 100
  * Return: Always 0 (Success)
  */
 int main(void)
 
 {
-	int a = 2;
-	int b = 1;
+	int i = 48;
+	int m = 48;
 
-
-	a = 0;
-	while (a < b)
-		printf("%d\n", a++);
+	while (i < 58)
+	{
+		while (m < 58)
+		{
+			putchar(i);
+			putchar(m++);
+			if (m != 58 || i != 58)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		m = 48;
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
