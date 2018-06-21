fbc38a94d754625b5699ffc72d7db1cb12444538
alex farioletti <gsf.nyc@gmail.com>
2018-06-21 12:54:10 -0700

    back to no match example

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,19 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int m;
-
-	m = n % 10;
-	if (m == 0)
+	if (n == 0)
 	{
-		printf("Last digit of %i is %i and is 0\n", n, m);
+		printf("Last digit of %i is and is 0\n", n);
 	}
-	else if (m > 5)
+	else if (n > 5)
 	{
-		printf("Last digit of %i is %i and is greater than 5\n", n, m);
+		printf("Last digit of %i is and is greater than 5\n", n);
 	} else
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %i is and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
