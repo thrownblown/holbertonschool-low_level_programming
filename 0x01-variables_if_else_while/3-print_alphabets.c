1e7a28e686fc18d6fa9d83f8d93c4c43b50cbdcb
alex farioletti <gsf.nyc@gmail.com>
2018-06-21 10:45:33 -0700

    make all the files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *  prints the sizes of various data types
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int a = 2;
+	int b = 1;
+
+
+	a = 0;
+	while (a < b)
+		printf("%d\n", a++);
+	return (0);
+}
