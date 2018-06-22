e4745e2faa5daee1502d96e1fbdf046f706ae664
alex farioletti <gsf.nyc@gmail.com>
2018-06-21 21:04:42 -0700

    prints all the nums

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - prints three numbers according to a bunch of weird rules
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i = 48;
+	int n = 48;
+	int m = 48;
+
+	while (i < 58)
+	{
+		while (m < 58)
+		{
+			if (i != m)
+			{
+				while (n < 58)
+				{
+					if (i != n && m != n)
+					{
+						putchar(i);
+						putchar(m);
+						putchar(n);
+						if (m != 56 ||  (i != 55 || n!= 57))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					n++;
+				}
+			}
+			m++;
+			n = m;
+		}
+		i++;
+		m = i;
+	}
+	putchar('\n');
+	return (0);
+}
