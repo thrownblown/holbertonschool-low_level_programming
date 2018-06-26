c93600f480450360321c83270f3878529af8a9ae
alex farioletti <gsf.nyc@gmail.com>
2018-06-26 09:20:14 -0700

    my own abs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include "6-abs.c"
+
 /**
  * print_last_digit - prints and returns the last digit of a supplied val.
  * @n: The number to evaluate.
@@ -12,11 +12,10 @@ int print_last_digit(int n)
 	int r;
 
 	if (n < 0)
-		r = _abs(n);
+		r = n * -1;
 	else
 		r = n;
 	r = r  % 10;
-	
 	_putchar('0' + r);
 	return (r);
 }
