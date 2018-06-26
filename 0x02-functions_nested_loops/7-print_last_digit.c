165d8e5e15b2ebbe633c087232b9d4fb93926fd8
alex farioletti <gsf.nyc@gmail.com>
2018-06-26 08:24:12 -0700

    back to test for posi

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include "6-abs.c"
 /**
  * print_last_digit - prints and returns the last digit of a supplied val.
  * @n: The number to evaluate.
@@ -10,28 +11,12 @@ int print_last_digit(int n)
 {
 	int r;
 
-	if (n < 0 ) 
+	if (n < 0)
 		r = _abs(n);
 	else
 		r = n;
-
 	r = r  % 10;
 	
 	_putchar('0' + r);
 	return (r);
-}
-/**
- * _abs - computes the absolute value of an integer.
- * @n: The number to evaluate.
- *
- * Return: abs val of n
- *
- */
-int _abs(int n)
-{
-	if (n > 0)
-		return (n);
-	else if (n < 0)
-		return (n * -1);
-	return (0);
 }
