f3593312192859ebca277a931e809166984dfb60
alex farioletti <gsf.nyc@gmail.com>
2018-06-26 07:49:11 -0700

    is it the return

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include "6-abs.c"
+/**
+ * print_last_digit -  prints and returns the last digit of a supplied val.
+ * @n: The number to evaluate.
+ *
+ * Return: last digit
+ *
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n  % 10;
+	r = _abs(r);
+	_putchar('0' + r);
+	return (r);
+}
