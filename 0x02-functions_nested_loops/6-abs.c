df4a700e81bbdeb446720f08769c29b5bef61582
alex farioletti <gsf.nyc@gmail.com>
2018-06-25 22:01:14 -0700

    prints the vals, absolutly

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: The number to evaluate.
+ *
+ * Return: abs val of n
+ *
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return(n * -1);
+	return (0);
+}
