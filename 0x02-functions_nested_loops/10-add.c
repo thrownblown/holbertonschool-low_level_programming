a706b122436b89207fe664631c797030a703d2c0
alex farioletti <gsf.nyc@gmail.com>
2018-06-26 11:11:47 -0700

    it wasn't that easy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - performs addition on two int inputs.
+ * @a: the first num to add.
+ * @b: the 2nd num to add.
+ *
+ * Return: the value of the above addition
+ *
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
