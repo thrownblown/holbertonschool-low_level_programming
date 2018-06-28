eef88e358aae56166921439c8d28f5394dc55da0
alex farioletti <gsf.nyc@gmail.com>
2018-06-28 07:34:44 -0700

    factors and products

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - performs multiplication on two int inputs.
+ * @a: the first factor.
+ * @b: the 2nd factor.
+ *
+ * Return: the product
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
