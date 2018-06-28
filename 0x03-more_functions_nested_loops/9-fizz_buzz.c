bf5a2df9e7219a9c287ade32f480bd59e260bf01
alex farioletti <gsf.nyc@gmail.com>
2018-06-28 12:38:56 -0700

    curls

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,25 @@ int main(void)
 	while (i++ < 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%d", i);
+		}	
 		if (i != 100)
+		{
 			printf(" ");
+		}
 	}
 	printf("\n");
 }
