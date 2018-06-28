d9038bc7386377d5740c2057f4984eefa032850b
alex farioletti <gsf.nyc@gmail.com>
2018-06-28 07:29:42 -0700

    too simple

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isdigit - determines if a character is a number
+ * @c: The character to evaluate
+ *
+ * Return: 1 if digit, otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	return (0);
+}
