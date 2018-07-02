a783b94354fb1ca00b1cce9f16a2bb4237f6dc7d
alex farioletti <gsf.nyc@gmail.com>
2018-07-02 16:12:46 -0700

    reseting

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - updates the vales stored at the supplied address to 98
+ * @n: The address to update to the value 98
+ *
+ * return: nothing void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
