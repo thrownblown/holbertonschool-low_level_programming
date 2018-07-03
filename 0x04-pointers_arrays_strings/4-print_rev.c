340d04a83e199cd77b3ede2a09385ac7063456ef
alex farioletti <gsf.nyc@gmail.com>
2018-07-03 08:59:45 -0700

    revert to most checks

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,38 +1,19 @@
 #include "holberton.h"
-
-/**
- * _strlen - calcs the len of a str
- * @s: pointer to the string to calc
- *
- * Return: the length of the str
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
+#include "2-strlen.c"
 /**
  * print_rev - prints a string to the terminal, backwards!
  * @s: string to print
  *
  * Return: void just prints
  */
-
 void print_rev(char *s)
 {
 	int i;
-	int j;
-	
-	j = _strlen(s);
-	for (i = j; i >= 0; i--)
+
+	i = _strlen(s);
+	while (i >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(s[i--]);
 	}
 	_putchar('\n');
 }
