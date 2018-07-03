f476442ef84a20cfb8a3838332c6579de551dceb
alex farioletti <gsf.nyc@gmail.com>
2018-07-03 14:04:23 -0700

    passes betty, matches example

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints a specified number of character from an array
+ * @a: array to print
+ * @n: number of characters to print
+ *
+ * Return: void just prints
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n - 1)
+		printf("%i, ", a[i++]);
+	printf("%i\n", a[i]);
+}
