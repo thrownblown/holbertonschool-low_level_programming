32cc822832916682ff7de49e6b21abf1da17ef8e
alex farioletti <gsf.nyc@gmail.com>
2018-07-03 11:25:01 -0700

    only half

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * puts_half - prints 2nd half of a string to the terminal
+ * @str: string to print
+ *
+ * Return: void just prints
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	i = _strlen(str);
+	if (i % 2 != 0)
+	{
+		i = (i - 1) / 2; 
+	}
+	else
+	{
+		i = i / 2;
+	}
+	
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
