9418534e302edf3201ea111c7d2cd925aae1a2d2
alex farioletti <gsf.nyc@gmail.com>
2018-07-02 17:06:56 -0700

    does it backwards

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * print_rev - prints a string to the terminal, backwards!
+ * @s: string to print
+ *
+ * Return: void just prints
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+	while (i >= 0)
+		_putchar(s[i--]);
+	_putchar('\n');
+}
