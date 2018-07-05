da6ef6c4cfc0a1a3155127658ea1a7cba3d8f78f
alex farioletti <gsf.nyc@gmail.com>
2018-07-05 14:51:07 -0700

    passes betsy

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _atoi - turn a str into an int
+ * @s: string to eval
+ * Return: n the value of the first number in the st0ring
+ */
+int _atoi(char *s)
+{
+	int i, n, posi;
+
+	i = 0;
+	n = 0;
+	posi = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			posi *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = s[i] - '0' + n * 10;
+			if (s[i] < '0' || s[i] > '9' || s[i] == '\0')
+				break;
+		}
+		i++;
+	}
+	return (n * posi);
+}
+
