e2fb428ce038d437dbcb8b4b9bf11415ef5a22fa
alex farioletti <gsf.nyc@gmail.com>
2018-07-07 10:45:37 -0700

    and less than n

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strcat - cat two strings towgether.
+ * @dest: target for appemdment
+ * @src: source string to be appended
+ *
+ * Return: pointer to the dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < n)
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
+}
