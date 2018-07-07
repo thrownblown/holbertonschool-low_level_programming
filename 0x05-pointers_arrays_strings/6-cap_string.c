fda02f350b7a1dae31f00da0f2e7dbe52c25200b
alex farioletti <gsf.nyc@gmail.com>
2018-07-07 16:14:05 -0700

    tickled by switch

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * cap_string - uppercases the first letter of every word
+ * @s: a string to uppity
+ *
+ * Return: string now upper
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		switch (s[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+			default:
+				i++;
+		}
+	}
+	return (s);
+}
