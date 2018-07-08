27e8dc026407eea6d0720b9db2dde451f650e498
alex farioletti <gsf.nyc@gmail.com>
2018-07-08 08:00:49 -0700

    133zy d03z i7

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * leet - changes a string to leetspeak
+ * @s: a string to leet
+ *
+ * Return: string now leeted
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char let = { 'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L' }
+	char num = { '4', '3', '0', '7', '1', '4', '3', '0', '7', '1' }
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (j < 9)
+		{
+			if (s[i] == let[j])
+				s[i] = num[j];
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
