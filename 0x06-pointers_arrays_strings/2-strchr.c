224a981e580114d2606ed9c8ce479e02d78b6954
alex farioletti <gsf.nyc@gmail.com>
2018-07-10 10:31:06 -0700

    llo ld

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: where to start looking
+ * @c: values to find
+ *
+ * Return: pointer to begining of mem area
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (0);
+}
