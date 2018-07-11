e03e0ae977627d6e8bda4ed1d4f63d000a946d0d
alex farioletti <gsf.nyc@gmail.com>
2018-07-11 12:26:36 -0700

    passes the betsy

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 
 /**
+ * _puts_recursion - prints a string to the term followed by nes line
+ * @s: string to put
  *
+ * Return: void i give you nothing
  */
 void _puts_recursion(char *s)
 {
 	if (s[0] != '\0')
 	{
-		
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
