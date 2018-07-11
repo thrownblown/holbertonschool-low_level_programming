c63ada675aa64729ec3f6c7bfb53545d285c35f9
Alex Farioletti <alex@tcbcourier.com>
2018-07-11 20:15:57 +0000

    return recurse

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _str_len_recursion - calculates the len of an str recursivly
+ * @s: string to len
+ *
+ * Return: len of str
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+		
+	}
+	return (0);
+}
