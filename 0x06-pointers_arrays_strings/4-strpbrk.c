06d7348eec75a9b4376b5a1dffb4722661aa5b70
alex farioletti <gsf.nyc@gmail.com>
2018-07-10 21:14:43 -0700

    need to test betsy

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strpbk -searches a string for any of a set of bytes.
+ * @s: string to look at
+ * @accept: string of acceptable chars
+ *
+ * Return: number of bytes in init seg that consist of bytes only from accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+				return (&s[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
