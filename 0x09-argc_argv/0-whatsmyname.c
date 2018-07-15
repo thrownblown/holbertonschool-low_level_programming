84f416592797af132dd013be735f8bc34df20f8c
alex farioletti <gsf.nyc@gmail.com>
2018-07-15 16:50:52 -0700

    betty approved

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program to the terminal
+ * @argc: count of command line
+ * @argv: the command line args
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
