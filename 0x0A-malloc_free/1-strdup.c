4e61fe16e6c47c4f1c925edd1256008901cbb2f7
alex farioletti <gsf.nyc@gmail.com>
2018-07-17 11:44:57 -0700

    dups the str

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string
+ * @str: str to cpy
+ *
+ * Return: pointer to new str or 0 if err
+ */
+
+char *_strdup(char *str)
+{
+		char *ar;
+		int i, size;
+
+		size = sizeof(str) / sizeof(str[0]);
+
+		ar = malloc(sizeof(char) * size);
+
+		if (!ar)
+			return (0);
+
+		for (i = 0; i <= size; i++)
+		{
+			ar[i] = str[i];
+		}
+		putchar(ar[i]);
+
+		return (ar);
+}
