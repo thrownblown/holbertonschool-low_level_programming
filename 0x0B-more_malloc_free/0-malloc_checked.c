1cf6ea88c6e2dbb785d3deb6a9ae8ee3692dbed9
alex farioletti <gsf.nyc@gmail.com>
2018-07-19 11:08:03 -0700

    bety

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /**
+ * malloc_checked - allocates mem
+ * @b: how much mem to malloc
  *
- *
+ * Return: pointer to our mem
  */
 
 void *malloc_checked(unsigned int b)
 {
-    unsigned int *bigb;
+	unsigned int *bigb;
 
 	bigb = malloc(b);
-	if (!bigb)
+	if (bigb == NULL)
 		exit(98);
 	return (bigb);
 }
