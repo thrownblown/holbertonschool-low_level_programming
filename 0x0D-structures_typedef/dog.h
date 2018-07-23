f3a9e6ceeba3edde64565e48763ed79b4b860caa
alex farioletti <gsf.nyc@gmail.com>
2018-07-23 13:43:32 -0700

    betty ok

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+  * struct dog - makes a dog
+  * @name: string for name
+  * @owner: doggy daddy (or mommy)
+  * @age: int for age, human years
+  */
+
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	int age;
+}
+#endif
