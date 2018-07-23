32a6db7f71a4d7aa38490c52236b42a3cd4f3d59
alex farioletti <gsf.nyc@gmail.com>
2018-07-23 15:27:48 -0700

    betty oks

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - pprints a donggy name, its age (human years) amd owner
+ * @d: puppers to print
+ *
+ * Return: nada
+ */
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: %s\n Age: %.6f\n Owner: %s\n", d.name, d.age, d.owner);
+}
