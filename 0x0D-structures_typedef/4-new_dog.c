de27dd2ee9920ec405bf112b46cf47c32da22c4a
alex farioletti <gsf.nyc@gmail.com>
2018-07-23 19:48:06 -0700

    dupe dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include <stdlib.h>
 #include "dog.h"
 
+
+/**
+ * _strdup - copies a string
+ * @str: str to cpy
+ *
+ * Return: pointer to new str or 0 if err
+ */
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = 0;
+	while (str[size] != '\0')
+		size++;
+
+	ar = (char *)malloc(size + 1);
+
+	if (!ar)
+		return (NULL);
+
+	for (; size >= 0; size--)
+		ar[size] = str[size];
+
+	return (ar);
+}
+
+
 /**
  * new_dog - fido factory
  * @name: our puppy nom de plume
@@ -13,10 +45,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *n, *o;
 
 	d = malloc(sizeof(dog_t));
-	if (!d)
-		return (NULL);
+	n = _strdup(name);
+	o = _strdup(owner);
+
+	if (!d || !n || !o)
+	{
+		free(d);
+		free(n);
+		free(o);
+		return (0);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
