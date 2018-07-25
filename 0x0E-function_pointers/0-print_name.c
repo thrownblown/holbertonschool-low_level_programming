49a1e1254c885e11adfa06c122fa9f02444c02b1
alex farioletti <gsf.nyc@gmail.com>
2018-07-25 10:29:41 -0700

    uppercases

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - supplies a variable to a funtion
+ * @name: var to give to print func
+ * @f: print func 
+ *
+ * Return: nope
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
