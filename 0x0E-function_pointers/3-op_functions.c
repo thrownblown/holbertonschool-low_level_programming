647a3dd5314b431924a4501dfd74800fba368ab3
alex farioletti <gsf.nyc@gmail.com>
2018-07-25 14:38:09 -0700

    baetty ok

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ * op_add - sums up a & b
+ * @a: int to add
+ * @b: other int to add
+ *
+ * Return: sum of a & b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts a & b
+ * @a: int to be removed from
+ * @b: other int to remove
+ *
+ * Return: difference of a & b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a & b
+ * @a: int to mul
+ * @b: other int to mul
+ *
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ * @a: int dividend
+ * @b: int divisor
+ *
+ * Return: quotient of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - performs a modulus op
+ * @a: int dividend
+ * @b: int divisor
+ *
+ * Return: remainder of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
