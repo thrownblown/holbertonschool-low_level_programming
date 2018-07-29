4546a5bde244e4830929c6e582e33c5d9471fdf7
Alex Farioletti <alex@tcbcourier.com>
2018-07-29 19:46:05 +0000

    test are broken

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef VARIADIC_H
+#define VARIADIC_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
