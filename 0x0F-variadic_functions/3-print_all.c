809755db1749867663391867f70de9697fac7750
alex farioletti <gsf.nyc@gmail.com>
2018-07-29 20:33:53 -0700

    init print all

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - function that prints anything
+ * @format: type of variable to print
+ *
+ * Return: nope just print
+ */
+
+void print_all(const char * const format, ...)
+{
+}
