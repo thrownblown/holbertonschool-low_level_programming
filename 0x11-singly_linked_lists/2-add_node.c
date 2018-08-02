92c5c0b789cb09ae0f5cba71267481423e783b99
alex farioletti <gsf.nyc@gmail.com>
2018-08-02 12:35:47 -0700

    passes test

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: start of list
+ * @str: to add
+ *
+ * Return: ptr to new head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
