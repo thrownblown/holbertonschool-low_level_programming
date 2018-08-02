6ba27d07235c5208288117e0b948142e0d180f3b
alex farioletti <gsf.nyc@gmail.com>
2018-08-02 11:46:10 -0700

    _putchar err

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -14,35 +14,9 @@ size_t print_list(const list_t *h)
 	for (i = 0; h; i++)
 	{
 		if (h->str && h->len)
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			/*
-			str = h->str;
-			_putchar('[');
-			_putchar('0' + h->len);
-			_putchar(']');
-			_putchar(' ');
-			for (j = 0; j < h->len; j++)
-			{
-				_putchar(str[j]);
-			}
-			*/
-		}
 		else
-		{
 			printf("[0] (nil)\n");
-			/*
-			_putchar('[');
-			_putchar('0');
-			_putchar(']');
-			_putchar(' ');
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-			*/
-		}
 		h = h->next;
 	}
 	return (i);
