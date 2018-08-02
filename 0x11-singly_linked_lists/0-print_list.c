2d41ebf93598a30d20e323d1915359bec900ee8f
alex farioletti <gsf.nyc@gmail.com>
2018-08-02 11:44:25 -0700

    stdio

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: start of list
@@ -9,13 +9,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t j, i;
-	char *str;
+	size_t i;
 
 	for (i = 0; h; i++)
 	{
 		if (h->str && h->len)
 		{
+			printf("[%d] %s\n", h->len, h->str);
+			/*
 			str = h->str;
 			_putchar('[');
 			_putchar('0' + h->len);
@@ -25,9 +26,12 @@ size_t print_list(const list_t *h)
 			{
 				_putchar(str[j]);
 			}
+			*/
 		}
 		else
 		{
+			printf("[0] (nil)\n");
+			/*
 			_putchar('[');
 			_putchar('0');
 			_putchar(']');
@@ -37,10 +41,9 @@ size_t print_list(const list_t *h)
 			_putchar('i');
 			_putchar('l');
 			_putchar(')');
+			*/
 		}
-		_putchar('\n');
 		h = h->next;
 	}
-	_putchar('\n');
 	return (i);
 }
