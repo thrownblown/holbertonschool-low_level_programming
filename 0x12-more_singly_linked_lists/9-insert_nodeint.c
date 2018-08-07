ea4b70b370ead1ede752101534df9b27405ad4f4
alex farioletti <gsf.nyc@gmail.com>
2018-08-07 14:00:37 -0700

    matches and passes

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * get_nodeint_at_index - node getter
+ * @h: start of list
+ * @index: position of node to be gotten
+ *
+ * Return: list node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *h, unsigned int index)
+{
+	size_t count;
+
+	for (count = 0; h != 0 && count < index; count++)
+		h = h->next;
+	return (h);
+}
+
+
+/**
+ * insert_nodeint_at_index - adds a new node at the end of a list_t list.
+ * @head: start of list
+ * @idx: position of insertion
+ * @n: value to set in new node
+ *
+ * Return: ptr to new head
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+	listint_t *h;
+
+	if (!head)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	h = get_nodeint_at_index(*head, idx);
+	new->next = h->next;
+	h->next = new;
+	return (new);
+}
