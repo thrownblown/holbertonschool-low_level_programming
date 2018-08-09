dfbec33b1993e4c38b7bc1be15832c28c4bd8824
alex farioletti <gsf.nyc@gmail.com>
2018-08-08 19:48:50 -0700

    check for get

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -54,7 +54,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *h;
 
-	if (!head)
+	if (!head || n == NULL)
 		return (NULL);
 	if (idx == 0)
 		return (add_nodeint(head, n));
@@ -71,6 +71,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (*head);
 	}
 	h = get_nodeint_at_index(*head, idx - 1);
+	if (!h)
+		return (NULL);
 	new->next = h->next;
 	h->next = new;
 	return (new);
