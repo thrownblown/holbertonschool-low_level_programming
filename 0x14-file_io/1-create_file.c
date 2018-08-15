1f7b7ef7b1e3dcd72fd9271bd777e04aa5107273
alex farioletti <gsf.nyc@gmail.com>
2018-08-15 10:59:25 -0700

    check 7 2

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,15 +15,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
 	if (text_content == NULL)
 	{
 		text_content = '\0';
+		close(file);
 		return (1);
 	}
-
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
 
 	while (text_content[i])
 	{
