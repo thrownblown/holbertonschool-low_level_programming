8335193ddafa25c16bc1a4abadc0c378eef5b534
alex farioletti <gsf.nyc@gmail.com>
2018-08-14 22:14:55 -0700

    matches test

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ *create_file- file maker pro
+ *@filename: title of file
+ *@text_content: content for file
+ *Return: -1 if err else 1
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, output, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		close(file);
+		return (1);
+	}
+
+	file = open(filename, O_TRUNC | O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	while (text_content[i])
+	{
+		output = write(file, &text_content[i], 1);
+		if (output == -1)
+			return (-1);
+		i++;
+	}
+
+	close(file);
+	return (1);
+}
