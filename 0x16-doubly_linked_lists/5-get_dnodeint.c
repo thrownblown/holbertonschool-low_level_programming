be59215052dbc0289b41821959021034ecd38055
Alex Farioletti <gsf.nyc@gmail.com>
2018-09-16 19:39:49 -0700

    forgot this one

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_dnodeint_at_index - node getter
+ * @h: start of list
+ * @index: position of node to be gotten
+ *
+ * Return: list node
+ */
+
+listint_t *get_dnodeint_at_index(listint_t *h, unsigned int index)
+{
+	size_t count;
+
+	for (count = 0; h != 0 && count < index; count++)
+		h = h->next;
+	return (h);
+}
