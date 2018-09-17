0e9fb7b48358eab09e32cbb3ef5a71f9213f37da
Alex Farioletti <gsf.nyc@gmail.com>
2018-09-16 18:37:34 -0700

    prev

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
+	new->prev = NULL
+	*head->prev = new;
 	*head = new;
 
 	return (new);
