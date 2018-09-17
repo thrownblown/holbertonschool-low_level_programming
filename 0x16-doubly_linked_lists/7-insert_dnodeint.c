dec2ec1293d0f1ddb603511eb21735aef1eada25
Alex Farioletti <gsf.nyc@gmail.com>
2018-09-16 21:01:48 -0700

    h

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 	if (idx == 0)
-		return (add_dnodeint(&head, n));
+		return (add_dnodeint(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
