1f5d0e09b8a7b76cadeb69639725b14da789b1c2
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-12 16:42:21 -0700

    hashin

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 hashing algorithm
+ * @str: key to be hashed out
+ *
+ * Return: numeric hash of string
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while (c = *str++)
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return hash;
+}
