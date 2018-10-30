e85d634518dc5220a9dd2b43f947068b02d51926
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-30 10:33:22 -0700

    works

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *next, *tmp2, *tmp = *list;
+	while(tmp)
+	{
+		next = tmp->next;
+		if (tmp->prev && next && tmp->n > tmp->next->n)
+		{
+			tmp->prev->next = tmp->next;
+			tmp->next = next->next;
+			next->next = tmp;
+			next->prev = tmp->prev;
+			tmp->prev = next;
+			print_list(*list);
+		}
+		while(tmp->prev && tmp->n < tmp->prev->n)
+		{
+			tmp2 = tmp->prev;
+			tmp2->next = tmp->next;
+			tmp2->next->prev = tmp2;
+			tmp->prev = tmp->prev->prev;
+			tmp2->prev = tmp;
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			tmp->next = tmp2;
+			print_list(*list);
+		}
+		tmp = next;
+	}
+}
