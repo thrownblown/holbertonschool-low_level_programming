82ddf60b5a93027ce23828b987d3ee8ed2c99957
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-31 16:40:18 -0700

    header

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -18,11 +18,12 @@ typedef struct listint_s
 } listint_t;
 
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+void print_list(listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
