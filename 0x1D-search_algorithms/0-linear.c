ae3818583211779c300aed74879d50bf34c0e44d
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-24 09:19:54 -0800

    forloop

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,22 +1,27 @@
 #include "search_algos.h"
 
 /**
- * main - Entry point
+ * linear_search - searches for a value in an array of integers
+ * using the Linear search algorithm
+ *
+ * @array: list of ints to search
+ * @size: len of arr
+ * @value: int to be found
  *
  * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i;
 
-    if (!array)
-        return(-1);
-    while (i < size)
-    {
-        printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-        if (array[i] == value)
-            return(i);
-        i++;
-    }
-    return(-1);
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
