a36e7992328bc3bfbc593e960919d9c1870beb9a
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-24 17:31:37 -0800

    headers2

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
 #ifndef _SEARCH_H_
 #define _SEARCH_H_
+
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
